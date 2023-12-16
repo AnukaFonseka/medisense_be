407c44c322c5820b92ab083e312c51a8933f84a1
AnukaFonseka <akunafonseka@gmail.com>
2023-12-16 12:36:23 +0530

    created more models and added associations

--- index.js
+++ index.js
@@ -17,16 +17,57 @@ const routes = require("./routes/index.routes");
 app.use("/", routes);
 
 try {
-    db.Users.belongsTo(db.Roles, { as: "roles", foreignKey: "roleId"});
-    db.Roles.hasMany(db.Users, { as: "users", foreignKey: "roleId"});
+    db.Users.belongsTo(db.Roles, { as: "roles", foreignKey: "roleId", onDelete: "cascade"});
+    db.Roles.hasMany(db.Users, { as: "users", foreignKey: "roleId", onDelete: "cascade"});
     db.Tests.belongsTo(db.Users, {as: "user", foreignKey: "userId"});
     db.Users.hasMany(db.Tests, {as: "tests", foreignKey: "userId"});
     db.Agencies.belongsTo(db.Users, {as: "user", foreignKey: "userId"});
     db.Users.hasMany(db.Agencies, {as: "agencies", foreignKey: "userId"});
 
-    //M-M
+    db.Customers.belongsTo(db.Agencies, {as: "agency", foreignKey: "agencyId"});
+    db.Agencies.hasMany(db.Customers, {as: "customers", foreignKey: "agencyId"});
+    db.Customers.belongsTo(db.Countries, {as: "country", foreignKey: "countryId"});
+    db.Countries.hasMany(db.Customers, {as: "customers", foreignKey: "countryId"});
+    db.Customers.belongsTo(db.Jobs, {as: "job", foreignKey: "jobId"});
+    db.Jobs.hasMany(db.Customers, {as: "customers", foreignKey: "jobId"});
+    db.DropdownData.belongsTo(db.Dropdowns, {as: "dropdown", foreignKey: "dropdownId", onDelete: "cascade"});
+    db.Dropdowns.hasMany(db.DropdownData, {as: "dropdownData", foreignKey: "dropdownId", onDelete: "cascade"});
+
     db.Packages.belongsToMany(db.Tests, {through: "PackageTests", foreignKey: "pkgId", onDelete: "cascade"});
     db.Tests.belongsToMany(db.Packages, {through: "PackageTests", foreignKey: "testId", onDelete: "cascade"});
+
+    db.Admissions.belongsTo(db.Customers, {as: "customer", foreignKey: "customerId", onDelete: "cascade"})
+    db.Customers.hasMany(db.Admissions, {as: "admissions", foreignKey: "customerId", onDelete: "cascade"});
+
+    db.Admissions.belongsTo(db.DropdownData, {as: "bank", foreignKey: "bankId"});
+    db.DropdownData.hasMany(db.Admissions, {as: "bank_admission", foreignKey: "bankId"});
+    db.Admissions.belongsTo(db.DropdownData, {as: "creditAprover", foreignKey: "creditAproverId"});
+    db.DropdownData.hasMany(db.Admissions, {as: "creditAprover_admission", foreignKey: "creditAproverId"});
+    db.Admissions.belongsTo(db.DropdownData, {as: "miniLabStatus", foreignKey: "miniLabStatusId"});
+    db.DropdownData.hasMany(db.Admissions, {as: "miniLabStatus_admission", foreignKey: "miniLabStatusId"});
+    db.Admissions.belongsTo(db.DropdownData, {as: "labStatus", foreignKey: "labStatusId"});
+    db.DropdownData.hasMany(db.Admissions, {as: "labStatus_admission", foreignKey: "labStatusId"});
+    db.Admissions.belongsTo(db.DropdownData, {as: "xRayStatus", foreignKey: "xRayStatusId"});
+    db.DropdownData.hasMany(db.Admissions, {as: "xRayStatus_admission", foreignKey: "xRayStatusId"});
+
+    db.CustomerTests.belongsTo(db.Customers, {as: "customer", foreignKey: "customerId", onDelete: "cascade"});
+    db.Customers.hasMany(db.CustomerTests, {as: "customerTests", foreignKey: "customerId", onDelete: "cascade"});
+    db.CustomerTests.belongsTo(db.Tests, {as: "test", foreignKey: "testId", onDelete: "cascade"});
+    db.Tests.hasMany(db.CustomerTests, {as: "customerTests", foreignKey: "testId", onDelete: "cascade"})
+    db.CustomerTests.belongsTo(db.Admissions, {as: "admission", foreignKey: "admissionId", onDelete: "cascade"});
+    db.Admissions.hasMany(db.CustomerTests, {as: "customerTests", foreignKey: "admissionId", onDelete: "cascade"});
+
+    db.CustomerPackages.belongsTo(db.Admissions, {as:"admission", foreignKey:"admissionId", onDelete: "cascade"});
+    db.Admissions.hasMany(db.CustomerPackages, {as: "customerPackages", foreignKey:"admissionId", onDelete: "cascade"})
+    db.CustomerPackages.belongsTo(db.Customers, {as:"customer", foreignKey:"customerId", onDelete: "cascade"});
+    db.Customers.hasMany(db.CustomerPackages, {as: "customerPackages", foreignKey:"customerId", onDelete: "cascade"})
+    db.CustomerPackages.belongsTo(db.Packages, {as:"package", foreignKey:"packageId", onDelete: "cascade"});
+    db.Packages.hasMany(db.CustomerPackages, {as: "customerPackages", foreignKey:"packageId", onDelete: "cascade"})
+
+    
+
+
+
 } catch (error) {
     console.log(error);
 }
--- models/Customers.js
+++ models/Customers.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         dateOfBirth: {
             type: DataTypes.DATEONLY,
-            allowNull: false,
+            allowNull: true,
         },
         sex: {
             type: DataTypes.STRING,
